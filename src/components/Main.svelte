fa0c44d5b58a5bb29f1e0571e3861c1ab14ab3dd
Lucas Marchand <56175142+LucasMarchnd@users.noreply.github.com>
2024-09-16 18:30:55 +0200

    Initial commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,138 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {name: 'Movie recommendation based on Netflix customer reviews', icon:'fa-solid fa-clapperboard',href:"https://www.google.com"},
+        {name: 'Chess engine', icon:'fa-solid fa-chess-board',href:"https://www.google.com"},
+        {name: 'StrasMemory', icon:'fa-solid fa-map-location-dot',href:"https://www.google.com"},
+        {name: 'TCP client-proxy-server communication with error detection', icon:'fa-solid fa-server',href:"https://www.google.com"},
+        {name: '2D and 3D Games with UE4', icon:'fa-solid fa-gamepad',href:"https://www.google.com"},
+        {name: 'Creation of a physics engine', icon:'fa-solid fa-scale-unbalanced',href:"https://www.google.com"},
+
+    ]
+
+    import TableRow from "./TableRow.svelte";   
+
+    let rows = [
+        {
+            title: 'Game Development',
+            icons: [
+                { src: 'images/icons/unrealengine.svg', alt: 'Unreal Engine 4' },
+                { src: 'images/icons/sdl.svg', alt: 'SDL2' },
+                { src: 'images/icons/cplusplus.svg', alt: 'C++' },
+                { src: 'images/icons/unity.svg', alt: 'Unity (Clicker app)' },
+                { src: 'images/icons/c.svg', alt: 'Love2D' },
+                { src: 'images/icons/sfml.svg', alt: 'SFML' }
+            ]
+        },
+        {
+            title: 'Web Development',
+            icons: [
+                { src: 'images/icons/html5.svg', alt: 'HTML' },
+                { src: 'images/icons/css3.svg', alt: 'CSS' },
+                { src: 'images/icons/javascript.svg', alt: 'JavaScript' },
+                { src: 'images/icons/svelte.svg', alt: 'Svelte' }
+            ]
+        },
+        {
+            title: 'Artificial Intelligence',
+            icons: [
+                { src: 'images/icons/python.svg', alt: 'Python3' },
+                { src: 'images/icons/tensorflow.svg', alt: 'Tensorflow' },
+                { src: 'images/icons/numpy.svg', alt: 'Numpy' }
+            ]
+        },
+        // Add more rows as needed...
+    ];
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Lucas</span> Marchand <br/>
+                <span class="poppins text-violet-400"> AI Student</span> 
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                Bla <span class="text-violet-400">Blabla</span> Blablabla <br/>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam animi ducimus dolorem possimus eum adipisci excepturi maiores facilis magni modi?
+            </p>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/pp.webp"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
+        </div>
+    </section>
+
+
+    <section id="projets" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class = "flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                <p class="underline">Mes projets :</p>
+            </h6>
+        </div>
+
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+            
+            <Step step={steps[1]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+            
+            <Step step={steps[2]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+
+            <Step step={steps[3]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+            
+            <Step step={steps[4]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+
+            <Step step={steps[5]}>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores expedita odit ratione laboriosam maiores aliquam facere beatae doloremque assumenda porro!
+                    <strong class="text-violet-400">Mettre des choses importantes</strong>
+                </p>
+            </Step>
+        </div>
+    </section>
+
+    <section id="Expérience" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
+        <div class = "flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                <p class="underline">Mon Expérience :</p>
+            </h6>
+        </div>
+
+        
+        {#each rows as row}
+        <TableRow title={row.title} icons={row.icons} />
+        <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
+        {/each}
+    </section>
+    <section id="cv" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col items-center gap-8 bg-slate-950">
+        <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold text-center">
+            Download my resume
+        </h2>
+    
+        <div class="flex justify-center mt-4">
+            <a href="/path_to_your_cv.pdf" download class="bg-white text-black py-3 px-6 rounded-lg shadow hover:bg-gray-200 transition duration-300">
+                Download my resume
+            </a>
+        </div>
+    </section>
+</main>
---
+++ src/components/TableRow.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let title;
+    export let icons; 
+</script>
+
+<div class="flex justify-between items-center py-4">
+    <div class="text-left">
+        <h3 class="text-xl font-bold">{title}</h3>
+    </div>
+
+    <div class="lg:flex lg:space-x-4 grid grid-cols-2 gap-4 md:grid-cols-4">
+        {#each icons as icon}
+            <div class="flex flex-col items-center">
+                <img src={icon.src} alt={icon.alt} class="w-10 h-10" style="filter: invert(1);">
+                <p class="text-white mt-2">{icon.alt}</p>
+            </div>
+        {/each}
+    </div>
+</div>
